fa7973071da504bd33256bb61204089d911d7976
Bob den Otter <bobdenotter@gmail.com>
2013-03-11 21:59:55 +0100

    Adding extension to github.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-tweetwidget
-===========
+Sea of Clouds - Twitter Widget
+=======================
 
-Tweetwidget is a twitter widget, based on the 'Sea of Clouds' Tweet! script.
+A twitter widget, based on the 'Sea of Clouds' script. The most often used
+options can be configured in the `config.yml` file. If you wish to change either
+the default CSS or the default javascript, you should copy the files from
+`SeaOfCloudsWidget/assets` to another file in that same folder. That way, they
+will not be overwritten when you update the extension.
+
+<pre>
+
+</pre>
+
+
+
+
---
+++ extension.php
@@ -0,0 +1,77 @@
+<?php
+// Sea of Clouds - Twitter Widget Extension for Bolt, by Bob den Otter
+
+namespace SeaOfCloudsWidget;
+
+use Bolt\Extensions\Snippets\Location as SnippetLocation;
+
+class Extension extends \Bolt\BaseExtension
+{
+
+    /**
+     * Info block for Sea of Clouds - Twitter Widget Extension.
+     */
+    function info()
+    {
+
+        $data = array(
+            'name' => "Tweet! - Twitter Widget",
+            'description' => "Tweetwidget is a twitter widget, based on the 'Sea of Clouds' Tweet! script.",
+            'keywords' => "twitter, widget, sidebar",
+            'author' => "Bob den Otter",
+            'link' => "http://bolt.cm",
+            'version' => "0.1",
+            'required_bolt_version' => "1.1.0",
+            'highest_bolt_version' => "1.1.1",
+            'type' => "Widget",
+            'first_releasedate' => "2013-03-10",
+            'latest_releasedate' => "2013-03-10",
+            'dependencies' => "",
+            'priority' => 10
+        );
+
+        return $data;
+
+    }
+
+    /**
+     * Initialize Sea of Clouds - Twitter Widget. Called during bootstrap phase.
+     */
+    function initialize()
+    {
+
+        // Make sure jQuery is included
+        $this->addJquery();
+
+        // Add javascript file
+        $this->addJavascript("assets/jquery.tweet.js");
+
+        // Add CSS file
+        $this->addCSS("assets/" . $this->config['style_filename']);
+
+        // Initialize the Twig function
+        $this->addTwigFunction('tweetwidget', 'tweetwidget');
+
+    }
+
+    /**
+     * Twig function {{ tweetwidget() }} in Sea of Clouds - Twitter Widget extension.
+     */
+    function tweetwidget()
+    {
+
+        $this->app['twig.loader.filesystem']->addPath(__DIR__);
+
+        $formhtml = $this->app['twig']->render("assets/" . $this->config['template'], array(
+            "config" => $this->config
+        ));
+
+        return new \Twig_Markup($formhtml, 'UTF-8');
+
+    }
+
+
+
+}
+
+
